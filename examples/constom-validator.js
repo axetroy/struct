a7156b769fe266f0a6a48967b82dd5e4266c5b34
Axetroy <troy450409405@gmail.com>
2017-11-24 23:39:25 +0800

    Update constom-validator.js

--- examples/constom-validator.js
+++ examples/constom-validator.js
@@ -12,7 +12,7 @@ const data = {
   email: 'xxx@axetroy.com'
 };
 
-const struct = truct({
+const struct = Struct({
   name: Struct.type.string,
   age: Struct.type.int,
   email: Struct.type.string.email // check string first, and then check email
